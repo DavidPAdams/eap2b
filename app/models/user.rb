d867e5a296d5fb150ebdfcec765cf05ce6201777
David Adams <david.riding.rails@gmail.com>
2020-04-10 14:27:07 -0700

    update user model format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   scope :last_updated, -> { order(updated_at: :desc) }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   before_save :downcase_email
 
   validates :name, presence: true
+  
   validates :email, presence: true, 
                     fromat: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  self.per_page = 25
 
   private
     def downcase_email
