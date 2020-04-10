f00eb754a04a0f9f01eb5b714eac79bd5cfd1801
David Adams <david.riding.rails@gmail.com>
2020-04-10 16:41:20 -0700

    change scope name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  scope :last_updated, -> { order(updated_at: :desc) }
+  scope :latest_updated, -> { order(updated_at: :desc) }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save :downcase_email
 
   validates :name, presence: true
-  
+
   validates :email, presence: true, 
                     fromat: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
