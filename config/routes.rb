1c672d46b9c11614d998997d7eb73c64828f7446
David Adams <david.riding.rails@gmail.com>
2020-04-10 17:13:54 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/delete'
-  get 'users/create'
-  get 'users/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users
 end
