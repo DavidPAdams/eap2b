c3ba0946a6242701ae94ca454ffae38f49c6467d
David Adams <david.riding.rails@gmail.com>
2020-04-12 20:14:52 -0700

    update to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all.latest_updated
@@ -17,6 +18,20 @@ class UsersController < ApplicationController
   end
 
   def delete
+    gone_user = @user.name
+    @user.destroy
+    flash[:success] = "#{gone_user} was deleted from the list"
+    
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :title, :status)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
 end
